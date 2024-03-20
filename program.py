ed1cad6ddf2f9438b1e5104688ad67642660d1e3
Avishek Sapkota Sharma <71004558+avishek2002@users.noreply.github.com>
2024-03-20 15:21:03 +1100

    added functionality to test if name attributes are empty in program.py

--- program.py
+++ program.py
@@ -1,8 +1,12 @@
 # This is the first program file of our project. It was created in the Update1.0 branch.
 # Later we opened a pull request and merged it to the main branch.
 
+# Update1.1 extended to check if name is valid or not.
+
 # user inputs first name and last name
 fname = input("Enter your first name : ")
 lname = input("Enter your last name : ")
-
-print("Hello, " fname + " " + lname)
+if (len(fname) == 0 or len(lname) == 0):
+	print("ERROR! First name and/or last name cannot be left empty!")
+else:
+	print("Hello, " + fname + " " + lname)
